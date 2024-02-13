aa3e71734c6f529b113424eb1cebb3675ebd1bd5
KseniiaHorai <horaiksenia@gmail.com>
2024-02-13 18:33:17 +0200

    Add HTTP handler for JSON time response

--- server.go
+++ server.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		currentTime := time.Now().Format(time.RFC3339)
+		response := map[string]string{"time": currentTime}
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonResponse)
+	})
+
+	http.ListenAndServe(":8795", nil)
+}
