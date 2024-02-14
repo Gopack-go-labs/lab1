9dcbb0dd479ee95bdac97f02d7dd2f0722dff3d7
Halai Andrii <halaiandriy@gmail.com>
2024-02-14 21:57:51 +0200

    Move getTime function to handers

--- server.go
+++ server.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"net/http"
-	"time"
+	// "time"
 )
 
 const PORT = ":8795"
 
 func main() {
-	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
-		currentTime := time.Now().Format(time.RFC3339)
-		response := map[string]string{"time": currentTime}
-		jsonResponse, err := json.Marshal(response)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(jsonResponse)
-	})
+	http.HandleFunc("/time", getTime)
 
   http.ListenAndServe(PORT, nil)
 }
